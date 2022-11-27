b33bb4374b0e8c4d33163e536edcc97d37170d26
Taleb Hammoudé <taleb.hammoude@outlook.com>
2022-11-28 00:25:56 +0100

    Signup component from function to class

--- homework3/src/Components/Signup/index.js
+++ homework3/src/Components/Signup/index.js
@@ -4,63 +4,71 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-import { useState } from "react";
+import React, { useState } from 'react';
 import {createUserWithEmailAndPassword} from 'firebase/auth';
 import { auth } from "../../Firebase";
-import { useNavigate } from 'react-router-dom';
+
 import ErrorModal from "../ErrorModal";
 
 
-const Signup = ()=> {
+class Signup extends React.Component {
 
 
-  const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [showModal, setShowModal] = useState(false);
-  const [errorText, setErrorText] = useState("");
+ 
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: '',
+      showModal: false,
+      errorText: ''
+    }
+  }
 
   
-
-  
-  const handleCloseModal = () => setShowModal(false);
-  const handleShowModal = () => setShowModal(true);
+  handleCloseModal = () => this.setState({showModal: false});
+  handleShowModal = () => this.setState({showModal: true});
   
 
 
-  const signUp = ()=> {
-    createUserWithEmailAndPassword(auth, email, password)
-    .then(auth => {navigate('/');})
-    .catch(error => {console.log(error); handleShowModal(); setErrorText("E-postadressen finns redan, välj en annan!")})
+  signUp = ()=> {
+    createUserWithEmailAndPassword(auth, this.state.email, this.state.password)
+    .then(auth => {window.location.replace("/");})
+    .catch(error => {console.log(error); this.handleShowModal();  this.setState({errorText: "E-postadressen finns redan, välj en annan!"})})
   }
 
 
+  render(){
+
     return (
-        <Container>
-        <ErrorModal showmodal = {showModal}  handleCloseModal={handleCloseModal}  errorText={errorText}/>
-        <Row>
-          <Col xs></Col>
-          <Col xs={{ order: 12 }}>
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Skapa ny e-postadress</Form.Label>
-                <Form.Control onChange={(event)=>{setEmail(event.target.value)}} type="email" placeholder="Ange e-postadress" />
-              </Form.Group>
-  
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Välj ett lösenord</Form.Label>
-                <Form.Control onChange={(event)=>{setPassword(event.target.value)}} type="password" placeholder="Ange ett lösenord" />
-              </Form.Group>
-  
-              <Button onClick={signUp} variant="warning">
-                Skapa kontot!
-              </Button>
-            </Form>
-          </Col>
-          <Col xs={{ order: 1 }}></Col>
-        </Row>
-      </Container>
-    );
+      <Container>
+      <ErrorModal showmodal = {this.state.showModal}  handleCloseModal={this.handleCloseModal}  errorText={this.state.errorText}/>
+      <Row>
+        <Col xs></Col>
+        <Col xs={{ order: 12 }}>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Skapa ny e-postadress</Form.Label>
+              <Form.Control onChange={(event)=>{this.setState({email:event.target.value })}} type="email" placeholder="Ange e-postadress" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Välj ett lösenord</Form.Label>
+              <Form.Control onChange={(event)=>{this.setState({password:event.target.value })}} type="password" placeholder="Ange ett lösenord" />
+            </Form.Group>
+
+            <Button onClick={this.signUp} variant="warning">
+              Skapa kontot!
+            </Button>
+          </Form>
+        </Col>
+        <Col xs={{ order: 1 }}></Col>
+      </Row>
+    </Container>
+  );
+  }
+    
 
 }
 
